07b324ac533ee79be707ddaebc084b3416ed3e90
allen-fang <allenfang08@gmail.com>
2017-09-20 16:49:01 -0700

    fixed prod keys

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,6 @@
 // production keys
 
-modules.exports = {
+module.exports = {
 	googleClientID: process.env.GOOGLE_CLIENT_ID,
 	googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
 	mongoURI: process.env.MONGO_URI,
