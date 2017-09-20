e6695f5e3b5348d32c91e318461438f3925c56f1
allen-fang <allenfang08@gmail.com>
2017-09-20 16:44:47 -0700

    finished oauth flow

---
+++ routes/authRoutes.js
@@ -0,0 +1,27 @@
+const passport = require('passport');
+
+module.exports = app => {
+	// route to authorize login
+	app.get(
+		'/auth/google',
+		passport.authenticate('google', {
+			prompt: 'consent',
+			scope: ['profile', 'email']
+		})
+	);
+
+	// callback route after logging in on google
+	app.get('/auth/google/callback', passport.authenticate('google'));
+
+	// route handler for logging out
+	// logout automicatlly bound to passport req object.
+	app.get('/api/logout', (req, res) => {
+		req.logout();
+		res.redirect('/');
+	});
+
+	// call to get currently logged in user
+	app.get('/api/current_user', (req, res) => {
+		res.send(req.user);
+	})
+};
