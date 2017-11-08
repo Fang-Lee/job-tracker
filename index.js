2de9d827345ebb78312e09fce02e4f39b75e9f25
allen-fang <allenfang08@gmail.com>
2017-11-08 14:18:48 -0800

    serves index.html on production

--- index.js
+++ index.js
@@ -34,9 +34,16 @@ require('./routes/authRoutes')(app);
 require('./routes/oppRoutes')(app);
 require('./routes/bucketRoutes')(app);
 
-app.get('/', (req, res) => {
-	res.status(200).send('hello world!');
-});
+// make sure express serves React routes correctly, only run in production.
+if (process.env.NODE_ENV === 'production') {
+	// Express serves up production assets (ex. main.js file or main.css file)
+	app.use(express.static('client/build'));
+	// Express will serve up index.html file if doesn't recognize the route
+	const path = require('path');
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
